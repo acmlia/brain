62754ae363eb06fb18c9b78e0e23ab744dab6ce3
acmlia <lia.meteorologia@gmail.com>
2019-02-04 09:48:33 -0200

    Atualizando ExtractRegion

--- brain.py
+++ brain.py
@@ -44,19 +44,29 @@ class BRain:
             print("Loading dataframe from file: ", file)
             dataframe = self.LoadCSV(self.IN_CSV_LIST, file)
             print("Extracting region from file '{}' using LAT limits: '{}' and LON limits: '{}'".format(file, self.LAT_LIMIT, self.LON_LIMIT))
+            filename = os.path.splitext(file)[0]
             subset = np.where(
                     (dataframe['lat']<=self.LAT_LIMIT[1]) &
                     (dataframe['lat']>=self.LAT_LIMIT[0]) &
                     (dataframe['lon']<=self.LON_LIMIT[1]) &
                     (dataframe['lon']>=self.LON_LIMIT[0]))
-            
+            dataframe_regional=dataframe.copy()
+            dataframe_regional=dataframe.iloc[subset]
+            dataframe_regional.drop(['numpixs'], axis=1, inplace=True)
+            subname=filename[9:15]
+            fname="Regional_BR_"+subname+"_var2d.csv"
+            dataframe_regional.to_csv(os.path.join(self.OUT_CSV_LIST, filename),index=False,sep=",",decimal='.')
+            print("The file ", fname ," was genetared!")
+        
+
     def PrintSettings(self):
         print(self.__dict__)
 
 
 mybrain = BRain()
 mybrain.PrintSettings()
-
+mybrain.LoadCSV()
+mybrain.ExtractRegion()
 # essa é uma mensagem para eu do futuro. Git é muito maneiro!!!
 
 
